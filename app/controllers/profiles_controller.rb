fe03075d2b5e1d081afa7170a8b5fb4d81eb10a0
Viktor <Poteshkin94@mail.ru>
2019-02-22 22:09:50 +0500

    Создание подписки

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,45 @@ before_action :set_user
   def show
   end
 
+  def subscribe
+      if current_user.id == @user.id
+        redirect_to profile_path(@user), notice: "Вы не можете подписаться на себя"
+      else
+
+        if current_user.subscription.exists?(friend_id: @user.id)
+          redirect_to profile_path(@user), notice: "Вы уже подписаны на данного пользователя"
+
+        else
+
+        @subscription = current_user.subscription.build
+        @subscription.friend_id = @user.id
+          if @subscription.save
+            redirect_to profile_path(@user), notice: "Вы успешно подписаны на данного пользователя"
+          end
+        end
+      end
+    end
+
+  def unsubscribe
+
+    if current_user.id == @user.id
+      redirect_to profile_path(@user), notice: "Вы не можете подписаться на себя"
+    else
+
+      if current_user.subscription.exists?(friend_id: @user.id)
+
+        @subscription = current_user.subscription.find_by_friend_id(@user.id)
+        @subscription.destroy
+        redirect_to profile_path(@user), notice: "Вы больше не  подписаны на данного пользователя"
+
+      else
+
+        redirect_to profile_path(@user), notice: "Вы не были подписаны на данного пользователя"
+          end
+        end
+    end
+
+
 private
 
   def set_user
