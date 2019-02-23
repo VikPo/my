c0e21962ec75c1d6a571d9afb3645a401cb0e4cf
Viktor <Poteshkin94@mail.ru>
2019-02-23 13:08:35 +0500

    Добавление name для user.email и настройка валидаций

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,9 @@
 class Photo < ApplicationRecord
   belongs_to :user
-   has_attached_file :image, style: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, style: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates :description, presence: true
+  validates :image, presence: true 
+
+
 end
